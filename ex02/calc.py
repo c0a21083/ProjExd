a85032f26b877cad300066acd0383f6155a7f85d
Yua <c0a210830e@edu.teu.ac.jp>
2022-11-29 14:49:40 +0900

    ex2-6

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,7 +2,7 @@ import tkinter as tk;
 import tkinter.messagebox as tkm;
 
 i=9;
-x=0
+x=1
 y=0
 
 root = tk.Tk()
@@ -10,11 +10,19 @@ root = tk.Tk()
 root.title("電卓");
 root.geometry("300x500");
 
+entry = tk.Entry(root, justify="right", width=10, font=("",40));
+entry.grid(row=0, column=0, columnspan=3);
+
 def button_click(event):
     btn = event.widget;
     num = btn["text"];
-    tkm.showinfo("押しましたね", f"{num}のボタンが押されました");
+    #entry.insert(tk.END, num);
+    #tkm.showinfo("押しましたね", f"{num}のボタンが押されました");
     
+    if num == "=":
+        pass
+    else:
+        entry.insert(tk.END, num)
 
 for i in range(9, -1, -1):
     button = tk.Button(root, font = ("", 30), text=f"{i}", width=4, height=2);
