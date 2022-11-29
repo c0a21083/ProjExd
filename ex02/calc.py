806cd7e70e33d40329f1d7863bfad4e11e885223
Yua <c0a210830e@edu.teu.ac.jp>
2022-11-29 14:31:14 +0900

    電卓

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,10 +1,26 @@
 import tkinter as tk;
 import tkinter.messagebox as tkm;
 
+i=9;
+x=0
+y=0
+
 root = tk.Tk()
 
 root.title("電卓");
 root.geometry("300x500");
 
-button = tk.Button(root, text="9", )
+for i in range(9, -1, -1):
+    button = tk.Button(root, font = ("", 30), text=i, width=4, height=2);
+    button.grid(row = x,column = y);
+    y += 1;
+    
+    if y%3==0:
+        x += 1;
+        y=0
+        
+    
+    
+
+root.mainloop();
     
