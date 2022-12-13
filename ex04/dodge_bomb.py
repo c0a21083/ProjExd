71a6d719a78daf2ba68f547b79199afa7f2535ae
Yua <c0a210830e@edu.teu.ac.jp>
2022-12-13 14:31:28 +0900

    P4

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -17,13 +17,28 @@ def main():
     tori_rct.center = 900, 400;
     scrn_sfc.blit(tori_sfc, tori_rct);
     
+    pg.display.update();
+    
     #Q2
     while True:
         scrn_sfc.blit(pgbg_sfc, pgbg_rct);
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return;
-    
+        
+        #キー入力の状態が入る辞書
+        #Q4
+        key_dict = pg.key.get_pressed();
+        if key_dict[pg.K_UP]:
+            tori_rct.centery -= 1;
+        if key_dict[pg.K_DOWN]:
+            tori_rct.centery += 1;
+        if key_dict[pg.K_LEFT]:
+            tori_rct.centerx -= 1;
+        if key_dict[pg.K_RIGHT]:
+            tori_rct.centerx += 1;
+        scrn_sfc.blit(tori_sfc, tori_rct);
+        
         pg.display.update();
         clock.tick(1000);
 
