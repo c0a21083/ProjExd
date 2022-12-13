4f5a86bdcf2d6245e32196fc5d16b9d64ba7c562
Yua <c0a210830e@edu.teu.ac.jp>
2022-12-13 14:23:44 +0900

    P2,3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,12 +2,33 @@ import pygame as pg;
 import sys;
 
 def main():
+    clock = pg.time.Clock();
     pg.display.set_caption("逃げろ！こうかとん！！");
     
     scrn_sfc = pg.display.set_mode((1600,900));
     
-    pgbg_sfc = pg.image.load("../fig/pg_bg.jpg");
+    pgbg_sfc = pg.image.load("../fig/pg_bg.jpeg");
+    pgbg_rct = pgbg_sfc.get_rect();
     
+    #Q3
+    tori_sfc = pg.image.load("../fig/6.png");
+    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0);
+    tori_rct = tori_sfc.get_rect();
+    tori_rct.center = 900, 400;
+    scrn_sfc.blit(tori_sfc, tori_rct);
+    
+    #Q2
+    while True:
+        scrn_sfc.blit(pgbg_sfc, pgbg_rct);
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return;
+    
+        pg.display.update();
+        clock.tick(1000);
 
 if __name__ == "__main__":
+    pg.init()
     main();
+    pg.quit;
+    sys.exit;
